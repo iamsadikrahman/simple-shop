95a149e04c97a65110b022480f5f497b890b6500
iamsadikrahman <iamsadikrahman@outlook.com>
2023-09-21 23:36:14 +0600

    responsive details page

--- src/views/ProductDetails.vue
+++ src/views/ProductDetails.vue
@@ -19,18 +19,18 @@ onBeforeMount(() => {
 </script>
 
 <template>
-    <section class="flex justify-center items-center h-screen space-x-36">
-    <div>
-    <img class="w-[300px]" :src="product.image" alt="">
+    <section class="flex sm:flex-row flex-col justify-center items-center md:h-screen">
+    <div class="w-6/12 ">
+    <img class="w-[300px] mx-auto" :src="product.image" alt="">
     </div>
-    <div class="flex flex-col space-y-5">
+    <div class="flex flex-col space-y-5 w-6/12">
         <div class="flex items-center space-x-5 font-semibold ">
         <span>Rating: {{ product.rating.rate }}</span>
         <span>{{ product.rating.count }} Reviews</span>
     </div>
     <h2 class="text-3xl font-semibold">{{product.title}}</h2>
     <h3 class="text-2xl font-semibold">${{product.price}}</h3>
-    <button class="w-2/12 bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">Buy Now</button>
+    <button class="w-[100px] bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded">Buy Now</button>
     <hr>
     <h4 class="text-xl font-semibold text-transform capitalize">{{product.category}}</h4>
     <hr>
